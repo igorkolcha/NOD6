ea18be3a1ea29dc4c6acc239c74c33aae8b693b6
Kolcha <igor.kolcha@list.ru>
2025-10-21 01:28:56 +0300

    added tabcontrol

--- NOD6/NOD6/Form1.Designer.cs
+++ NOD6/NOD6/Form1.Designer.cs
@@ -28,18 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(4, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(794, 150);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(786, 122);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(192, 72);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
             Name = "Form1";
             Text = "ШНД";
+            tabControl1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
--- NOD6/NOD6/obj/Debug/net8.0-windows/NOD6.AssemblyInfo.cs
+++ NOD6/NOD6/obj/Debug/net8.0-windows/NOD6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NOD6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93862e2e808a968e1d1e7e616f1edb150fb01b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd14d24a82e3dc413ae4831ca7f2611f16167b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("NOD6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NOD6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
